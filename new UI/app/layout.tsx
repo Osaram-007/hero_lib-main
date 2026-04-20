a02fdd1363fa29fe62f430227355ed1a93c0a39a
Osaram-007 <104356183+Osaram-007@users.noreply.github.com>
2026-04-20 12:18:32 +0530

    ok name

--- new UI/app/layout.tsx
+++ new UI/app/layout.tsx
@@ -6,7 +6,7 @@ import { NavBar } from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-    title: "Liberias",
+    title: "Liberia",
     description: "Modern Library Management System v2",
 };
 
