2a2495d69afe2701c28ebe4e797cfdc15c0cfb70
bpratik01 <pratikrbokade05@gmail.com>
2026-04-20 12:32:48 +0530

    pce

--- new UI/app/layout.tsx
+++ new UI/app/layout.tsx
@@ -6,7 +6,7 @@ import { NavBar } from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-    title: "Liberia",
+    title: "Hero Lib",
     description: "Modern Library Management System v2",
 };
 
